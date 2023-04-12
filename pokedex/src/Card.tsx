6e774fff92e2cdfecf80c02b82e2875559d4202f
Fabrício Bahiense Santos <fabriciobs2000@gmail.com>
2023-04-11 21:22:54 -0300

    fetch pokemoninfo funcionando

--- pokedex/src/Card.tsx
+++ pokedex/src/Card.tsx
@@ -1,13 +1,47 @@
+import { url } from 'inspector';
 import bulba from './img/bulba.png'
+import { useEffect, useState } from 'react';
 
+// Deixar aqui pra mapear os animados no futuro
 
+// declare type BlackWhite = {
+//     'front_default':String;
+// }
+// declare type SpriteVersions = {
+//     'generation-v': BlackWhite;
+// }
 
-declare type Pokemon = {
-    'name': String,
-    'url': String
+ class PokemonSprites {
+    'front_default': String;
 }
 
+ class PokemonInfo {
+    'name': String;
+    'sprites': PokemonSprites;
+}
+
+declare type Pokemon = {
+    'name': String;
+    'url': URL;
+}
+
+
 function Card ( props : { pokemon : Pokemon}) {
+    const [pokemonInfo, setPokemonInfo] = useState<PokemonInfo>(new PokemonInfo())
+
+    useEffect(()=> {
+        async function LoadInfo(callback: React.Dispatch<React.SetStateAction<PokemonInfo>>) {
+            const res = await fetch(props.pokemon.url);
+            const obj: PokemonInfo = await res.json()
+            
+            callback(obj)
+        }
+
+        LoadInfo(setPokemonInfo)
+
+    }, [])
+
+    
     return(
         <div className="card">
             <img src={bulba} alt="Bulbasaur" />
--- pokedex/src/ListContainer.tsx
+++ pokedex/src/ListContainer.tsx
@@ -1,33 +1,37 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Card from './Card'
 
 
 function ListContainer() {
 
     type Pokemon = {
-        'name': String,
-        'url': String
+        'name': String;
+        'url': URL;
     }
     type ApiResults = {
-        'results': Pokemon[]
+        'results': Pokemon[];
     }
-
     const [pokemons, setPokemons] = useState<Pokemon[]> (new Array<Pokemon>())
 
-    async function LoadPokemons(callback: React.Dispatch<React.SetStateAction<Pokemon[]>>) {
+    useEffect(()=> {
+        async function LoadPokemons(callback: React.Dispatch<React.SetStateAction<Pokemon[]>>) {
+    
+            const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=10&offset=0")
+            const obj: ApiResults = await res.json()
+    
+            callback(obj.results)
+        }
+        LoadPokemons(setPokemons)
 
-        const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=25&offset=0")
-        const obj: ApiResults = await res.json()
+    }, [])
 
-        callback(obj.results)
-    }
 
-    LoadPokemons(setPokemons)
+
 
 
     return(
         <div className="list bg-bg bg-[length:100%_100%] w-[60%] h-screen pl-10 pt-40 overflow-y-auto scrollbar">
-            { pokemons.map(p=> <Card pokemon={p} />) }
+            { pokemons.map((p, i)=> <Card pokemon={p} key={i}/>) }
         </div>    
     )
 }
